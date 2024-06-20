aea7f740f31065da62d4dbce1cdaf0df5a17b776
Ha Tai Thanh <wwehttdmqvgh@gmail.com>
2024-06-20 13:11:31 +0700

    Toi_uu_chinh_sua_toan_phan

--- Project_Do_an_co_so/src/project_do_an_co_so/BanHuyenLuyen/View_Toi_Uu.java
+++ Project_Do_an_co_so/src/project_do_an_co_so/BanHuyenLuyen/View_Toi_Uu.java
@@ -159,11 +159,11 @@ public class View_Toi_Uu extends JFrame {
                 }
 
                 // Calculate the quality score using the provided formula
-                double qi = 0.05 * metrics[0] + 10 * metrics[1] + 4 * metrics[2] + 2 * metrics[3] + 2 * metrics[4]
+                double qi = (0.05 * metrics[0] + 10 * metrics[1] + 4 * metrics[2] + 2 * metrics[3] + 2 * metrics[4]
                         - 5 * metrics[5] + 6 * metrics[6] + 0.2 * metrics[7] + 0.2 * metrics[8] + 2 * metrics[9]
                         + 2 * metrics[10] + metrics[11] - metrics[12] - metrics[13] - 2 * metrics[14]
                         - 5 * metrics[15] - metrics[16] + 4 * metrics[17] + 5 * metrics[18] + 5 * metrics[19]
-                        + 1.5 * metrics[20];
+                        + 2.5 * metrics[20]) * metrics[21];
 
                 Player player = new Player(playerName, qi);
 
@@ -236,7 +236,7 @@ public class View_Toi_Uu extends JFrame {
                 { 700, 330 }, { 700, 480 }, { 700, 180 },
                 { 600, 330 }, { 600, 480 }, { 600, 180 },
                 { 450, 600 }, { 450, 330 }, { 450, 480 }, { 450, 180 }, { 450, 50 },
-                { 200, 600 }, { 120, 600 }, { 150, 330 }, { 150, 480 }, { 150, 180 }, { 120, 50 }, { 200, 50 },
+                { 250, 600 }, { 150, 600 }, { 150, 330 }, { 150, 480 }, { 150, 180 }, { 150, 50 }, { 250, 50 },
                 { 300, 330 }, { 300, 480 }, { 300, 180 }
         };
 
@@ -264,6 +264,7 @@ public class View_Toi_Uu extends JFrame {
             });
             panel.add(button);
         }
+        selectedPositions.add("GK");
     }
 
     private class BackgroundPanel extends JPanel {
